1735363db9aa18cb8567af3d1400c0f742b3d00f
Moriya <moriyakatsman@hilma.tech>
2024-11-12 17:19:19 +0200

    signin and log in onchange functions properly

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-
+import { useState } from "react";
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
       <h1>Signup</h1>
       <h1>Login Here!</h1>
       <label htmlFor="username">Username:</label>
       <br></br>
-      <input type="text" className="username" value={username} />
+      <input type="text" className="username" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
       <br></br>
       <br></br>
       <label htmlFor="password">PassWord:</label>
       <br></br>
-      <input type="text" className="password" value={password} />
+      <input type="text" className="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
       <label htmlFor="password">Varify PassWord:</label>
       <br></br>
-      <input type="text" className="password" value={password} />
+      <input type="text" className="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
       <br></br>
       <br></br>
       <button>Sign Up</button>
