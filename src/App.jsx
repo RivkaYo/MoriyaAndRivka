7a88495bd0f1e9e5c0c53740dc491004c5a238ee
rivka <rivkamiller@hilma.tech>
2024-11-12 15:54:08 +0200

    add basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import React from "react";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/login" element={<h1>Login/</h1>>} />
+            <Route path="/signup" element={<h1>Signup</h1>} />
+            <Route path="/home" element={<h1>Home</h1>}>
+              <Route index element={<h1>Info</h1>} />
+              <Route path="todos" element={<h1>Todos</h1>} />
+              <Route path="posts" element={<h1>Posts</h1>} />
+              <Route path="albums" element={<h1>Albums</h1>}>
+                <Route path="photos" element={<h1>Photos</h1>} />
+              </Route>
+            </Route>
+          </Route>
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+      </Router>
     </>
   );
 }
