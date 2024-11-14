228335e9bf5c58baaa98c9958d5a4db4952c1e95
rivka <rivkamiller@hilma.tech>
2024-11-14 12:27:46 +0200

    add search to albums

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,10 +3,12 @@ import Completed from "./Completed";
 import DeleteBtn from "./DeleteBtn";
 
 const TodoItem = ({ item }) => {
+  console.log("item: ", item);
+  console.log("item.completed: ", item.completed);
   return (
     <div>
-      ID: {item.id} <br></br>
-      Title: {item.title}
+      ID: {item.id}: {item.title}
+      {item.completed}
       <Completed item={item} />
       <DeleteBtn item={item} />
     </div>
